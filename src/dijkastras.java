b48b82c94b540becf79f0b014551258eaca2d694
sarfarazalam13 <alamsarfaraz386@gmail.com>
2023-08-06 05:20:28 +0530

    dijkastras

--- src/dijkastras.java
+++ src/dijkastras.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class dijkastras {
@@ -13,13 +14,52 @@ class edge {
         this.wt = wt;
     }
 }
+
+class pair implements Comparable<pair>
+{
+    int n;
+    int path;
+    public pair(int n,int path)
+    {
+        this.n=n;
+        this.path=path;
+    }
+    public int compareTo(pair p2)
+    {
+        return this.path-p2.path;
+    }
+}
+void dij(ArrayList<edge>gra[],int src)
+{
+    int dist[]=new int[gra.length];
+    for(int i=0;i<gra.length;i++)
+    {
+        if(i!=src)
+        {
+            dist[i]=Integer.MAX_VALUE;
+        }
+    }
+    PriorityQueue<pair>pq=new PriorityQueue<>();
+    pq.add(new pair(src,0) );
+    boolean vis[]=new boolean[gra.length];
+
+    while (!pq.isEmpty())
+    {
+     pair curr=pq.remove();
+     if(!vis[curr.n])
+     {
+         vis[curr.n]=true;
+
+     }
+    }
+}
     void add(ArrayList<edge>gra[])
     {
            for(int i=0;i<gra.length;i++)
            {
                gra[i]=new ArrayList<>();
            }
-           Scanner in=new Scanner(System.in);
+           /*Scanner in=new Scanner(System.in);
            int i=0;
            while(i<gra.length)
            {
@@ -37,7 +77,22 @@ class edge {
                {
                    i++;
                }
-           }
+           }*/
+        gra[0].add(new edge(0,1,2));
+        gra[0].add(new edge(0,2,4));
+
+
+        gra[1].add(new edge(1,2,1));
+        gra[1].add(new edge(1,3,7));
+
+        gra[2].add(new edge(2,4,3));
+
+        gra[3].add(new edge(3,5,1));
+
+        gra[4].add(new edge(4,3,2));
+        gra[4].add(new edge(4,5,5));
+
+
 
     }
     void printlgra(ArrayList<edge>gra[])
@@ -58,7 +113,7 @@ class maaa
     public static void main(String args[])
     {
         dijkastras d=new dijkastras();
-        ArrayList<dijkastras.edge>gra[]=new ArrayList[5];
+        ArrayList<dijkastras.edge>gra[]=new ArrayList[6];
         d.add(gra);
         d.printlgra(gra);
     }
